a92793e27a01fedd4791e1d4f48a3dad3db605ba
Scott W Harden <swharden@gmail.com>
2023-01-24 19:06:10 -0500

    create unit test project

---
+++ src/FtdiSharpTests/UnitTests.cs
@@ -0,0 +1,13 @@
+﻿namespace FtdiSharpTests;
+
+public class UnitTests
+{
+    [Test]
+    public void Test_DeviceNames_CanBeParsed()
+    {
+        foreach (FtdiSharp.FTD2XX.FT_DEVICE dev in Enum.GetValues(typeof(FtdiSharp.FTD2XX.FT_DEVICE)))
+        {
+            Console.WriteLine(dev.ToReadableName());
+        }
+    }
+}

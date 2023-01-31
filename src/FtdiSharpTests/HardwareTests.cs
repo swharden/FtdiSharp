42b890116713153a9c7a11c90e28d64b47f110f4
Scott W Harden <swharden@gmail.com>
2023-01-30 23:47:19 -0500

    tests: I2C light sensor

--- src/FtdiSharpTests/HardwareTests.cs
+++ src/FtdiSharpTests/HardwareTests.cs
@@ -2,6 +2,8 @@ namespace FtdiSharpTests;
 
 public class HardwareTests
 {
+    readonly bool FtdiDeviceFound = FtdiDevices.Scan().Any();
+
     [Test]
     public void Test_Scan_Devices()
     {
@@ -12,7 +14,7 @@ public class HardwareTests
     [Test]
     public void Test_Scan_I2C_Addresses()
     {
-        if (!FtdiDevices.Scan().Any())
+        if (!FtdiDeviceFound)
             return;
 
         FtdiDevice device = FtdiDevices.Scan().First();
@@ -21,5 +23,28 @@ public class HardwareTests
 
         foreach (byte address in i2c.Scan())
             Console.WriteLine(address);
+    }
+
+    [Test]
+    public void Test_Read_Light_Sensor()
+    {
+        if (!FtdiDeviceFound)
+            return;
+
+        // Connect to the first USB FTDI device
+        FtdiDevice device = FtdiDevices.Scan().First();
+        FtdiSharp.Protocols.I2C i2c = new(device);
+
+        // Enter continuous sensor mode
+        byte address = 0x23;
+        byte config = 0b00010000;
+        i2c.Write(address, config);
+
+        // Read light intensity as two bytes
+        byte[] bytes = i2c.Read(address, 2);
+
+        // Convert the two bytes to lumens according to the datasheet
+        double value = (bytes[0] * 256 + bytes[1]) / 1.2;
+        Console.WriteLine(value);
     }
 }

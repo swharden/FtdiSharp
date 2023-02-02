9e4130207226a6d650bf2f3af0e97a92ab06e660
Scott W Harden <swharden@gmail.com>
2023-02-02 01:42:29 -0500

    Display: Binary(byte)

--- src/FtdiSharp/Display.cs
+++ src/FtdiSharp/Display.cs
@@ -8,4 +8,9 @@ public static class Display
     {
         return string.Join(", ", bytes.Select(x => Convert.ToString(x, 2).PadLeft(8, '0')));
     }
+
+    public static string Binary(this byte value)
+    {
+        return Binary(new byte[] { value });
+    }
 }

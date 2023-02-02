3cf62d975b52af66998c00cbdc02a342f10a5cae
Scott W Harden <swharden@gmail.com>
2023-02-02 01:43:52 -0500

    SPI: improve bidirectional transfer

--- src/FtdiSharp/Protocols/SPI.cs
+++ src/FtdiSharp/Protocols/SPI.cs
@@ -1,4 +1,5 @@
 ﻿using FtdiSharp.FTD2XX;
+using System.Net.NetworkInformation;
 
 namespace FtdiSharp.Protocols;
 
@@ -31,6 +32,9 @@ public class SPI : ProtocolBase
         // SCL Frequency (MHz) = 60 / ((1 + DIVISOR) * 2)
         UInt32 clockDivisor = 29; // for 1 MHz
 
+        // increase clock divisor to slow down signaling
+        //clockDivisor *= 100;
+
         byte[] bytes2 = new byte[]
         {
             0x80, // Set directions of lower 8 pins and force value on bits set as output
@@ -48,7 +52,7 @@ public class SPI : ProtocolBase
         Thread.Sleep(50);
     }
 
-    public void CsHigh()
+    public void CsHighDisable()
     {
         byte[] bytes = new byte[]
         {
@@ -59,7 +63,7 @@ public class SPI : ProtocolBase
         FtdiDevice.Write(bytes);
     }
 
-    public void CsLow()
+    public void CsLowEnable()
     {
         byte[] bytes = new byte[]
         {
@@ -76,21 +80,100 @@ public class SPI : ProtocolBase
 
     public byte[] ReadBytes(int length)
     {
-        CsLow();
+        CsLowEnable();
 
-        byte[] writeBuffer =
-        {
-            0x24, // MSB_FALLING_EDGE_CLOCK_BYTE_IN
-            0x01, // two bytes
-            0x00,
-        };
-        FtdiDevice.Write(writeBuffer);
+        byte lengthL = (byte)length;
+        byte lengthH = (byte)(length >> 8);
+        if (lengthH != 0)
+            throw new NotImplementedException();
 
-        byte[] readBuffer = new byte[2];
+        const byte DATA_IN_BYTES_FALLING_EDGE = 0x24; // app note section 3.3
+        //const byte DATA_IN_BYTES_RISING_EDGE = 0x20; // app note section 3.3
+
+        byte[] writeBytes = { DATA_IN_BYTES_FALLING_EDGE, lengthL, lengthH };
+        FtdiDevice.Write(writeBytes);
+
+        byte[] readBytes = new byte[length];
         uint bytesRead = 0;
-        FtdiDevice.Read(readBuffer, 2, ref bytesRead).ThrowIfNotOK();
+        FtdiDevice.Read(readBytes, (uint)length, ref bytesRead).ThrowIfNotOK();
 
-        CsHigh();
-        return readBuffer;
+        CsHighDisable();
+
+        return readBytes;
+    }
+
+    private static byte GetGpioMask(bool sck = false, bool mosi = false, bool miso = false, bool cs = false)
+    {
+        byte b = 0;
+
+        if (sck)
+            b |= (0 << 1);
+        if (mosi)
+            b |= (1 << 1);
+        if (miso)
+            b |= (2 << 1);
+        if (cs)
+            b |= (3 << 1);
+
+        return b;
+    }
+
+    /// <summary>
+    /// Pulse the clock line a certain number of times
+    /// </summary>
+    /// <param name="count"></param>
+    public void PulseClock(int count)
+    {
+        const byte CMD_SET_GPIO_STATE = 0x80; // Application Note AN_108 (section 3.6.1)
+        const byte GPIO_DIRECTION = 0b00001011; //  bit3:CS, bit2:MISO, bit1:MOSI, bit0:SCK
+
+        byte[] bytesClockLow = new byte[] { CMD_SET_GPIO_STATE, 0b11111111, GPIO_DIRECTION };
+        byte[] bytesClockHigh = new byte[] { CMD_SET_GPIO_STATE, 0b11111110, GPIO_DIRECTION };
+
+        for (int i = 0; i < count; i++)
+        {
+            FtdiDevice.Write(bytesClockLow);
+            FtdiDevice.Write(bytesClockHigh);
+        }
+    }
+
+    public byte ReadGpioLow()
+    {
+        const byte READ_GPIO_LOW = 0x81; // Application Note AN_108 (section 3.6.3)
+        const byte SEND_IMMEDIATE = 0x87;
+        FtdiDevice.Write(new byte[] { READ_GPIO_LOW, SEND_IMMEDIATE });
+
+        byte[] result = { 0 };
+        uint numBytesRead = 0;
+        FtdiDevice.Read(result, 1, ref numBytesRead).ThrowIfNotOK();
+
+        return result[0];
+    }
+
+    /// <summary>
+    /// Let the other guy do the clocking
+    /// </summary>
+    public byte[] RxData(uint BytesToRead)
+    {
+        uint NumBytesInQueue = 0;
+        uint TotalBytesRead = 0;
+        uint NumBytesRxd = 0;
+
+        byte[] InputBuffer = new byte[BytesToRead];
+
+        while (TotalBytesRead < BytesToRead)
+        {
+            FtdiDevice.GetRxBytesAvailable(ref NumBytesInQueue);
+
+            if (NumBytesInQueue == 0)
+                continue;
+
+            FtdiDevice.Read(InputBuffer, NumBytesInQueue, ref NumBytesRxd);
+
+            if (NumBytesInQueue == NumBytesRxd)
+                return InputBuffer;
+        }
+
+        return InputBuffer;
     }
 }
